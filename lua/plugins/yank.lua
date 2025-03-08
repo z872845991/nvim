cf1345ad6593f06d73089e96903b29f399a01b44
Jesse Wang <z872845991@163.com>
2025-03-08 19:05:40 +0800

    default and ready for deprecated

--- lua/plugins/yank.lua
+++ lua/plugins/yank.lua
@@ -16,22 +16,20 @@ return {
 							select = '<cr>',
 							paste = '<c-p>',
 							paste_behind = '<c-k>',
-							replay = '<c-q>',  -- replay a macro
-							delete = '<c-d>',  -- delete an entry
-							edit = '<c-e>',  -- edit an entry
+							replay = '<c-q>', -- replay a macro
+							delete = '<c-d>', -- delete an entry
+							edit = '<c-e>', -- edit an entry
 							custom = {},
-						  },
-						  n = {
+						},
+						n = {
 							select = '<cr>',
 							paste = 'p',
-							--- It is possible to map to more than one key.
-							-- paste = { 'p', '<c-p>' },
 							paste_behind = 'P',
 							replay = 'q',
 							delete = 'd',
 							edit = 'e',
 							custom = {},
-						  },
+						},
 					},
 				},
 			})
