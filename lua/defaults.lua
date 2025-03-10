85cd31f9a0741b7f50f45e44d63eac0597f1f5a3
Jesse Wang <z872845991@163.com>
2025-03-10 17:12:20 +0800

    clipboard

--- lua/defaults.lua
+++ lua/defaults.lua
@@ -1,17 +1,17 @@
 -- Set options
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true --show relative line numbers
-vim.opt.autoindent = true -- Enable auto-indentation
-vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for (when 'expandtab' is not set).
-vim.opt.shiftwidth = 4  -- Number of spaces to use for each step of (auto)indent
-vim.opt.smarttab = true -- Use shiftwidths at left margin, tabstops everywhere else
-vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
-vim.opt.mouse = "a"   -- Enable mouse support
-vim.opt.modifiable = true -- Allow buffer to be modified
-vim.opt.encoding = "UTF-8" -- Set encoding to UTF-8
+vim.opt.number = true                    -- Show line numbers
+vim.opt.relativenumber = true            --show relative line numbers
+vim.opt.autoindent = true                -- Enable auto-indentation
+vim.opt.tabstop = 4                      -- Number of spaces that a <Tab> in the file counts for (when 'expandtab' is not set).
+vim.opt.shiftwidth = 4                   -- Number of spaces to use for each step of (auto)indent
+vim.opt.smarttab = true                  -- Use shiftwidths at left margin, tabstops everywhere else
+vim.opt.softtabstop = 4                  -- Number of spaces that a <Tab> counts for while editing
+vim.opt.mouse = "a"                      -- Enable mouse support
+vim.opt.modifiable = true                -- Allow buffer to be modified
+vim.opt.encoding = "UTF-8"               -- Set encoding to UTF-8
 vim.opt.completeopt = "menuone,noselect" -- Set completion options
 ---@diagnostic disable-next-line: undefined-field
-vim.opt.completeopt:remove("preview") -- Remove preview from completion options
+vim.opt.completeopt:remove("preview")    -- Remove preview from completion options
 -- vim.api.nvim_set_hl(0, "String", {ctermfg = 76}) -- Set string color to cyan
 -- vim.api.nvim_set_hl(0, "Number", {ctermfg = 76}) -- Set number color to cyan
 
@@ -23,7 +23,7 @@ vim.cmd [[
 
 -- Set folding settings use by ufo provider
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
@@ -32,10 +32,7 @@ vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
 vim.opt.undodir = os.getenv("HOME") .. "/.local/.nvim_redo"
 vim.opt.undofile = true
 
--- -- Autocommands for folding using AnyFold plugin
--- vim.api.nvim_create_autocmd("Filetype", {
---     pattern = "*",
---     callback = function()
---         vim.cmd "AnyFoldActivate"
---     end
--- })
+-- Setup clipboard
+-- if vim.fn.has("clipboard") then
+-- 	vim.opt.clipboard = "unnamedplus"
+-- end
