2e2bc8565db591fdb9d0e25345dc6430b6b25214
Jesse Wang <z872845991@163.com>
2025-03-21 14:38:52 +0800

    disable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"github/copilot.vim",
 		config = function()
+			vim.g.copilot_enabled = false
 			vim.cmd('imap <silent><script><expr> <Tab> copilot#Accept("")')
 			vim.cmd([[
 			let g:copilot_filetypes = {
