1445b4b3a698a235ed2616e93cc3e3a3ce06f397
Jesse Wang <z872845991@163.com>
2025-03-28 11:39:12 +0800

    not use vim-table-mode

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -44,13 +44,13 @@ return {
 			vim.api.nvim_set_keymap('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', { noremap = true })
 		end
 	},
-	{
-		"dhruvasagar/vim-table-mode",
-		ft = { "markdown" },
-		config = function()
-			vim.api.nvim_set_keymap('n', '<leader>mt', ':TableModeToggle<CR>', { noremap = true })
-		end
-	},
+	-- {
+	-- 	"dhruvasagar/vim-table-mode",
+	-- 	ft = { "markdown" },
+	-- 	config = function()
+	-- 		vim.api.nvim_set_keymap('n', '<leader>mt', ':TableModeToggle<CR>', { noremap = true })
+	-- 	end
+	-- },
 	{
 		'mzlogin/vim-markdown-toc',
 		ft = { "markdown" },
@@ -67,5 +67,16 @@ return {
                 nnoremap <leader>mc :UpdateToc<CR>
             ]]
 		end
-	}
+	},
+	{
+		"Zeioth/markmap.nvim",
+		build = "yarn global add markmap-cli",
+		cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+		opts = {
+			html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+			hide_toolbar = false,     -- (default)
+			grace_period = 3600000    -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+		},
+		config = function(_, opts) require("markmap").setup(opts) end
+	},
 }
