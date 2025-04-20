7d2e7ae336117ac5e23b94c81c2c50536a3503e6
Jesse Wang <z872845991@163.com>
2025-04-20 10:05:26 +0800

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -26,6 +26,9 @@ return {
 		priority = 1000,
 		opts = {
 		},
+		-- config = function()
+		-- 	vim.cmd.colorscheme "tokyonight-night"
+		-- end,
 	},
 	{
 		'ray-x/aurora',
@@ -36,7 +39,11 @@ return {
 		end,
 		config = function()
 			vim.cmd.colorscheme "aurora"
-			-- vim.api.nvim_set_hl(0, '@number', { fg = '#e933e3' })
+			vim.api.nvim_set_hl(0, '@number', { fg = '#e933e3' })
+
+			vim.api.nvim_set_hl(0, "Search", { fg = "#c0caf5", bg = "#3d59a1", bold = true })
+			vim.api.nvim_set_hl(0, "IncSearch", { fg = "#15161e", bg = "#FF5555", bold = true })
+			vim.api.nvim_set_hl(0, "CurSearch", { fg = "#15161e", bg = "#FF5555", bold = true })
 		end
 	}
 }
